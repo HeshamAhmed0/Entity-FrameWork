12f0a7abf0a494a1cdfa61f65c99cb404c157413
Hesham Mathana <heshammathana0@gmail.com>
2025-03-05 02:46:00 +0200

    Entity FrameWork

---
+++ Session01/Mapping in EF/Mapping in EF/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mapping_in_EF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
