d8f4f7ab6ca4f804118e466ef2685ac3fbd406f9
Hesham Mathana <heshammathana0@gmail.com>
2025-03-06 23:35:24 +0200

    Connect database with App With Way By Convention

--- Session01/Mapping in EF/Mapping in EF/DbContexts/CustomerDbContext.cs
+++ Session01/Mapping in EF/Mapping in EF/DbContexts/CustomerDbContext.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mapping_in_EF.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mapping_in_EF.DbContexts
 {
-    internal class DbContext
+    internal class CustomerDbContext :DbContext
     {
+        public CustomerDbContext():base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = Employee ;Trusted_Connection =true");
+        }
+        public DbSet<Employees> Employee { get; set; }
     }
 }
