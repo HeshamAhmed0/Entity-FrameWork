2633c8dc805f48b503ed41c2e568cfd895eacbd9
Hesham Mathana <heshammathana0@gmail.com>
2025-03-06 23:35:06 +0200

    Connect database with App With Way By Convention

--- Session01/Mapping in EF/Mapping in EF/Program.cs
+++ Session01/Mapping in EF/Mapping in EF/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Mapping_in_EF
+﻿using Mapping_in_EF.DbContexts;
+
+namespace Mapping_in_EF
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Create Object From DbContext
+
+            //using CustomerDbContext dbContext = new CustomerDbContext();
+            // Some Code 
+
+
+
+            //CustomerDbContext dbContext = new CustomerDbContext();
+            // try
+            // {
+            //     //Some code
+            // }
+            // finally
+            // {
+            //     //Some code 
+            //     // in this this way this code will Run Any Way 
+            // } 
+            #endregion
+
+            #region Mapping By Convention 
+
+            #endregion
         }
     }
 }
