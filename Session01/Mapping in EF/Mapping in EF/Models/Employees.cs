9fe950451837a1470c7c641cd7f73e0a708f706b
Hesham Mathana <heshammathana0@gmail.com>
2025-03-06 23:35:37 +0200

    Connect database with App With Way By Convention

---
+++ Session01/Mapping in EF/Mapping in EF/Models/Employees.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapping_in_EF.Models
+{
+    internal class Employees
+    {
+        // Id Will Generate As Primary Key As => EmployeesId OR Id 
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Salary { get; set; }
+        public int Age { get; set; }
+    }
+}
