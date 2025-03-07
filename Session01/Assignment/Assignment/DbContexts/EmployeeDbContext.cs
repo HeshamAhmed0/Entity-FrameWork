4d441b341061c77dacb019764c4a393d52befc66
Hesham Mathana <heshammathana0@gmail.com>
2025-03-07 16:18:05 +0200

    Create Database With EntityFrameWorkCore (by Convention && Data Anotation)

---
+++ Session01/Assignment/Assignment/DbContexts/EmployeeDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.DbContexts
+{
+    internal class EmployeeDbContext :DbContext
+    {
+        public EmployeeDbContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = Employee_Details ; Trusted_Connection = true ; TrustServerCertificate = true ;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
