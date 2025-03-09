23088927b0c978e6e3b0ab2161e44b87ce02592b
Hesham Mathana <heshammathana0@gmail.com>
2025-03-09 16:21:36 +0200

    Updata-Database

---
+++ Sssion02/EFCore/EFCore/Migrtions/20250309121225_EditEmployee.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrtions
+{
+    /// <inheritdoc />
+    public partial class EditEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmpName",
+                table: "Employees",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmpName",
+                table: "Employees",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
