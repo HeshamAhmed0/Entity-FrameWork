c33b37edfe3bdecc2d813008808c768af49e201b
Hesham Mathana <heshammathana0@gmail.com>
2025-03-09 16:19:52 +0200

    Apply For Fluent Api

---
+++ Sssion02/EFCore/EFCore/DbContexts/EmployeesDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.DbContexts
+{
+    internal class EmployeesDbContext : DbContext
+    {
+        public EmployeesDbContext() : base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = Test ; Trusted_Connection = true ;TrustServerCertificate = True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasKey(E => E.Id);
+
+            modelBuilder.Entity<Employee>()
+                        .Property<string>("EmployeeName");
+
+            modelBuilder.Entity<Employee>()
+                        .Property(E => E.Name)
+                        .HasColumnType("varchar")
+                        .HasColumnName("EmpName")
+                        .HasMaxLength(50);
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
