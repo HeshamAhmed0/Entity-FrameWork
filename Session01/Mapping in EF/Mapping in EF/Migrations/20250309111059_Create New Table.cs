7919aa66d831a42f10699e0708c966419e883630
Hesham Mathana <heshammathana0@gmail.com>
2025-03-09 16:18:36 +0200

    Apply For Data Anotation

---
+++ Session01/Mapping in EF/Mapping in EF/Migrations/20250309111059_Create New Table.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mapping_in_EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateNewTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employee",
+                table: "Employee");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmpName",
+                table: "Employee",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Employee",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeName",
+                table: "Employee",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employee",
+                table: "Employee",
+                column: "EmpName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employee",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeName",
+                table: "Employee");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Employee",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmpName",
+                table: "Employee",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employee",
+                table: "Employee",
+                column: "Id");
+        }
+    }
+}
