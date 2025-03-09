f370760324453360342c846cd825a1c36c84a7a4
Hesham Mathana <heshammathana0@gmail.com>
2025-03-09 16:21:55 +0200

    Apply For Fluent Api

---
+++ Sssion02/EFCore/EFCore/Migrtions/20250309121634_TestFluentApi.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrtions
+{
+    /// <inheritdoc />
+    public partial class TestFluentApi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeName",
+                table: "Employees");
+        }
+    }
+}
---
+++ Sssion02/EFCore/EFCore/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Models
+{
+    internal class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(name: "EmpName",TypeName = "varchar")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public int Age { get; set; }
+
+    }
+}
