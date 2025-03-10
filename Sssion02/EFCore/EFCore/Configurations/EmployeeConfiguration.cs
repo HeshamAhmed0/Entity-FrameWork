8e4afe56c586024d9db2abe70a41df113b59cfd7
Hesham Mathana <heshammathana0@gmail.com>
2025-03-10 08:59:50 +0200

    Apply For Entry State

---
+++ Sssion02/EFCore/EFCore/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCore.Configurations
+{
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+           builder.HasKey(E => E.Id);
+
+          builder.Property<string>("EmployeeName");
+
+         builder.Property(E => E.Name)
+                        .HasColumnType("varchar")
+                        .HasColumnName("EmpName")
+                        .HasMaxLength(50);
+        }
+    }
+}
---
+++ Sssion02/EFCore/EFCore/Configurations/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCore.Configurations
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(P => P.Id).HasColumnName("Id");
+                
+        }
+    }
+}
