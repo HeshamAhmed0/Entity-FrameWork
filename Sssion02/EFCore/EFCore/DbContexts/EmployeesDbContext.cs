f41dbdb5015db99d1bc0bbf85b0175e36813906b
Hesham Mathana <heshammathana0@gmail.com>
2025-03-11 02:17:09 +0200

    Apply For Fluent Api

--- Sssion02/EFCore/EFCore/DbContexts/EmployeesDbContext.cs
+++ Sssion02/EFCore/EFCore/DbContexts/EmployeesDbContext.cs
@@ -18,19 +18,38 @@ namespace EFCore.DbContexts
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = .; Database = Etsh ; Trusted_Connection = true ;TrustServerCertificate = True;");
+            optionsBuilder.UseSqlServer("Server = .; Database = Test ; Trusted_Connection = true ;TrustServerCertificate = True;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
            
                        //modelBuilder.ApplyConfiguration<Employee>(new EmployeeConfiguration()); 
-                       modelBuilder.ApplyConfigurationsFromAssembly(assembly : Assembly.GetExecutingAssembly());
+                       //modelBuilder.ApplyConfigurationsFromAssembly(assembly : Assembly.GetExecutingAssembly());
             modelBuilder.ApplyConfigurationsFromAssembly(assembly: Assembly.GetExecutingAssembly());
 
+
+            // Foreign Key will initialize in Table Employee
+            //modelBuilder.Entity<Employee>()
+            //            .HasOne(E => E.Department)
+            //            .WithOne(D => D.E)
+            //            .HasForeignKey<Department>(F => F.DeptId);
+
+            // Foreign Key will initialize in Table department
+            //modelBuilder.Entity<Department>()
+            //            .HasOne(D => D.E)
+            //            .WithOne(E => E.Department)
+            //            .HasForeignKey<Employee>(f => f.Id);
+
+
+
+
+
+
         }
-       
+
 
         public DbSet<Employee> Employees { get; set; }
-        public DbSet<Product> Products { get; set; }
+        public DbSet<Addres> Addreses { get; set; } ///*NotValid*/
+
     }
 }
--- Sssion02/EFCore/EFCore/Models/Employee.cs
+++ Sssion02/EFCore/EFCore/Models/Employee.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +12,15 @@ namespace EFCore.Models
     {
         [Key]
         public int Id { get; set; }
-        [Column(name: "EmpName",TypeName = "varchar")]
+        [Column(name: "EmpName", TypeName = "varchar")]
         [MaxLength(50)]
         public string Name { get; set; }
         public decimal Salary { get; set; }
         public int Age { get; set; }
-
+        //[ForeignKey(nameof(Department))]     => Data Anotation 
+                        //Fluent Api 
+        //public int DepartmentDeptId { get; set; }
+       /* public Department? Department { get; set; } */  // Partial Participate
+        public Addres EmpAddres { get; set; }
     }
 }
