66423812935a0de21e690c9b97434e4c8e247b62
Hesham Mathana <heshammathana0@gmail.com>
2025-03-11 02:18:59 +0200

    Apply For Fluent Api

---
+++ Sssion02/EFCore/EFCore/Models/Addres.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Models
+{
+    [Owned]
+    internal class Addres
+    {
+        
+        public String? City { get; set; }
+        public String? Country { get; set; }
+        public String? Streat { get; set; }
+
+    }
+}
---
+++ Sssion02/EFCore/EFCore/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Models
+{
+    internal class Department
+    {
+        [Key]
+        public int DeptId { get; set; }
+        public Employee E { get; set; }  //Total Participate
+
+    }
+}
