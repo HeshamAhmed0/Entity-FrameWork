eee24335d32d9e05a9fc7f7de36ad8da04c80811
Hesham Mathana <heshammathana0@gmail.com>
2025-03-15 05:36:34 +0200

    New Freature Fon Insert In Database

---
+++ Session03/Session03/Session03/Program.cs
@@ -0,0 +1,28 @@
+﻿using Session03.DbContects;
+using Session03.Models;
+
+namespace Session03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AppDbContext appDbContext = new AppDbContext();
+
+            #region Manual seed Data
+            //List<Student> students = new List<Student>()
+            //{
+            //    new Student(){Name="hesham",Age=21},
+            //    new Student(){Name="hassan",Age=19},
+            //}; 
+
+            //appDbContext.Students.AddRange(students);
+            //appDbContext.SaveChanges();
+            #endregion
+
+
+
+        }
+    }
+}
