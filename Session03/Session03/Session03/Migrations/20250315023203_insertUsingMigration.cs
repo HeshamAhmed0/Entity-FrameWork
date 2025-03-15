4d4601ed88b46c0a35948eabbb23e63bbbc5ee5b
Hesham Mathana <heshammathana0@gmail.com>
2025-03-15 05:37:25 +0200

    Apply For Session03 EFCore

---
+++ Session03/Session03/Session03/Migrations/20250315023203_insertUsingMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Session03.Migrations
+{
+    /// <inheritdoc />
+    public partial class insertUsingMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Name" },
+                values: new object[] { 50, 20, "Khaled" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 50);
+        }
+    }
+}
