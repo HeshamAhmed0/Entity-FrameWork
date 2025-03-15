74a85f27c9133d4e26edf86834156402b4b89af0
Hesham Mathana <heshammathana0@gmail.com>
2025-03-15 05:37:56 +0200

    Apply For Session03 EFCore

---
+++ Session03/Session03/Session03/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session03.Models
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+        public ICollection<StudentCourse> StudentCourses { get; set; } = new HashSet<StudentCourse>();
+    }
+}
