f284fb502e09e6295c2cc03dc67fd11aac3de96a
Hesham Mathana <heshammathana0@gmail.com>
2025-03-18 14:00:39 +0200

    Apply For Session03 EFCore

--- Session03/Session03/Session03/Migrations/20250318103954_CreateStudentCourseTable.Designer.cs
+++ Session03/Session03/Session03/Migrations/20250318103954_CreateStudentCourseTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Session03.DbContects;
 
@@ -10,9 +11,11 @@ using Session03.DbContects;
 namespace Session03.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250318103954_CreateStudentCourseTable")]
+    partial class CreateStudentCourseTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Session03/Session03/Session03/Program.cs
+++ Session03/Session03/Session03/Program.cs
@@ -1,4 +1,5 @@
-﻿using Session03.DbContects;
+﻿using Microsoft.EntityFrameworkCore;
+using Session03.DbContects;
 using Session03.Models;
 
 namespace Session03
@@ -21,8 +22,33 @@ namespace Session03
             //appDbContext.SaveChanges();
             #endregion
 
+            #region EagerLoading
 
 
+
+
+            //var StudentCorse = appDbContext.Students.Include(E => E.StudentCourses).FirstOrDefault(E => E.Id == 4);
+            //Console.WriteLine(StudentCorse);
+
+
+            var Student01 = appDbContext.Set<StudentCourse>().Include(E => E.Student).FirstOrDefault(E => E.StdId == 4);
+
+            Console.WriteLine($"StudentId = {Student01?.StdId}");
+            Console.WriteLine(Student01?.CrsId);
+            Console.WriteLine(Student01?.Grade);
+            Console.WriteLine(Student01?.Student.Name);
+
+            //var StdCource =(from D in appDbContext.Students
+            //                    where D.Id==Student01.StdId
+            //                    select D).FirstOrDefault();
+            //    Console.WriteLine(StdCource.Id);
+            //    Console.WriteLine(StdCource.Name);
+            //    Console.WriteLine(StdCource.Age);
+
+
+
+
+            #endregion
         }
     }
 }
