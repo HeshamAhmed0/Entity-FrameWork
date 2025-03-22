d26573a91009ad499543c0fa61d732c33e1494d1
Hesham Mathana <heshammathana0@gmail.com>
2025-03-23 01:57:01 +0200

    in this Section The Start and ApplicationDbContext

---
+++ CenterApplication/Center/Center/ApplicationDbContexts/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Center.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Center.ApplicationDbContexts
+{
+    internal class AppDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = . ; Database = CodeTitans ;Trusted_Connection = true ;TrustServerCertificate = true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Constructor>().HasOne(E => E.Department)
+                                              .WithMany(D => D.Constructor)
+                                              .HasForeignKey(D => D.DeptId);
+
+            modelBuilder.Entity<Student>().HasOne(C => C.Constructor)
+                                          .WithMany(S => S.Students)
+                                          .HasForeignKey(C => C.ConstructorId);
+
+            
+        }
+        public DbSet<Constructor> Constructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ CenterApplication/Center/Center/Start.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Center.SelectStatement;
+using Center.Transaction;
+
+namespace Center
+{
+    internal class Start
+    {
+        int Input;
+        public void Decition()
+        {
+            Console.WriteLine("Please Choose What Do You Want ");
+            Console.WriteLine("1 :: Insert");
+            Console.WriteLine("2 :: Select");
+            Input = int.Parse(Console.ReadLine());
+            if (Input == 1)
+            {
+                InsertSection();
+            }
+            else if (Input == 2)
+            {
+                Console.Clear();
+                SelectFromDatabase selectFromDatabase = new SelectFromDatabase();
+                selectFromDatabase.Select();
+            }
+
+        }
+        public void InsertSection()
+        {
+
+            Console.Clear();
+            Console.WriteLine("1 :: InsertIntoConstructor");
+            Console.WriteLine("2 :: InsertIntoDepartment");
+            Console.WriteLine("3 :: InsertIntoStudent");
+            Input = int.Parse(Console.ReadLine());
+            Console.Clear() ;
+            if (Input == 1)
+            {
+                InsertIntoConstructor.InserConstructor();
+            }
+            else if (Input == 2)
+            {
+                InsertIntoDepartment.InsertDepartment();
+            }
+            else if (Input == 3)
+            {
+                InsertIntoStudents.InsertStudent();
+            }
+            else
+            {
+                Console.WriteLine("You Choosen Is Not Correct Please Choose Correct Choosen");
+                InsertSection();
+            }
+            AnotherDescition();
+
+        }
+        public void AnotherDescition()
+        {
+            Console.WriteLine("Do You Want Another Desction " +
+                              "1 :: Yes " +
+                              "2 :: No");
+            Console.Write("You Choosen Is : ");
+            Input = int.Parse(Console.ReadLine());
+            if (Input == 1)
+            {
+                Decition();
+            }
+            else
+            {
+                Console.WriteLine("See You Soon");
+            }
+        }
+    }
+}
