3dfec2c5104dc469b7002257300e34441245e7fd
Hesham Mathana <heshammathana0@gmail.com>
2025-03-23 01:58:01 +0200

    Make Models For Database

---
+++ CenterApplication/Center/Center/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Models
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int StudentAge { get; set; }
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+
+
+        [ForeignKey(nameof(Constructor))]
+        public int ConstructorId { get; set; }
+        public Constructor Constructor {  get; set; }
+        
+    }
+}
