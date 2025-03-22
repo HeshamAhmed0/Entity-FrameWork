42d3b50e46486aea011a15872118330c9f80c0dc
Hesham Mathana <heshammathana0@gmail.com>
2025-03-23 01:58:23 +0200

    Apply For Insert Statement

---
+++ CenterApplication/Center/Center/Transaction/InsertIntoDepartment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Center.ApplicationDbContexts;
+using Center.Models;
+
+namespace Center.Transaction
+{
+    internal static  class InsertIntoDepartment
+    {
+       public static void InsertDepartment()
+        {    
+            AppDbContext appDbContext=new AppDbContext();
+
+            Console.Write("Enter Department Name : ");
+            string DepartmentName = Console.ReadLine();
+            Department NewDepartment = new Department()
+            {
+                DepartmentName = DepartmentName
+            };
+            appDbContext.Add(NewDepartment);
+                appDbContext.SaveChanges();
+        }
+    }
+}
