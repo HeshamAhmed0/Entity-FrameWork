cfd8f64581a85f1085e4a03713771e7fecd890d4
Hesham Mathana <heshammathana0@gmail.com>
2025-03-31 10:03:20 +0200

    New Feature in my new Application

---
+++ Library Management System/Library Management System/Library Management System/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_Management_System.DbContexts
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = Library Management System ;Trusted_Connection = true ;TrustServerCertificate = true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasOne(D => D.Department)
+                                       .WithMany(B => B.Books)
+                                       .HasForeignKey(BD => BD.DepartmentId);
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Library Management System/Library Management System/Library Management System/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System.Models
+{
+    internal class Book
+    {
+        public int BookNumber { get; set; }
+        public string BookName { get; set; }
+        public decimal BookSalary { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        
+
+    }
+}
